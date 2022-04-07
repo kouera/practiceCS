50b3bfd8f2ae431504ad5ac7cf3ca8bd78ea881c
Mohamed Kouera <kouera@gmail.com>
2022-04-07 17:48:36 +0200

    some practices

--- Lektion-1/ConsoleApp_NET6/Program.cs
+++ Lektion-1/ConsoleApp_NET6/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Mohamed!");
+﻿/* Console.Write("enter a string ");
+string readInput = Console.ReadLine();
+Console.WriteLine("you have entred {0}",readInput);
+
+Console.Write(" Enter a string and press enter: ");
+int asciiValue = Console.Read();
+Console.WriteLine("ASCII value is {0}", asciiValue);
+Console.ReadKey()*/
+
+
+
+//Console.Write("Write your first name:  ");
+//String firstname = Console.ReadLine();
+
+//Console.Write("Write your last name:  ");
+//String lastname = Console.ReadLine();
+
+//string fullname = firstname + " " +lastname;
+//Console.WriteLine("your full name is: " + fullname);
+//Console.WriteLine("the lenghth of the string is "+ fullname.Length);
+
+
+
+
+//Console.WriteLine("enter your age");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("your age is : " + age + " years old");
+
+
+
+
+string fname = Console.ReadLine();
+if (fname == "")
+    Console.WriteLine("please enter a correct name: ");
+else
+    Console.WriteLine("Thank you");
+    
+return;
+
